41464c7e84eac286989d5512dbd19c1f14738ef3
marlenekoh <marlenekohxml@gmail.com>
2018-10-23 16:37:44 +0800

    add CsvDataBuilder class

---
+++ be/models/CsvDataBuilder.py
@@ -0,0 +1,15 @@
+'''
+Represents a builder class to build csv data from an uploaded csv file
+'''
+class CsvDataBuilder:
+    def __init__(self, infoType, dataDictionary, inputFile):
+        self.csvData = CsvData(dataDictionary, inputFile)
+
+    def setOrder:
+        for i in self.csvData.infoType:
+			if "author.csv" in i:
+			elif "review.csv" in i:
+			elif "submission.csv" in i:
+			else:
+				print ("ERROR: file should have been rejected by frontend already")
+
